ef08ffe6fd088e6f6f753b84ef6fed092e15e272
sprumin <sprumin123@gmail.com>
2019-10-02 17:54:34 +0900

    add Photos, Trashcan models

--- fanpage/models.py
+++ fanpage/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
+
 # Create your models here.
+class Photos(models.Model):
+    idx = models.AutoField(primary_key=True)
+    photo = models.ImageField(upload_to="image", unique=True)
+    title = models.CharField(max_length=64)
+    link = models.CharField(max_length=2048)
+    source = models.CharField(max_length=2048)
+    size = models.CharField(max_length=16)
+    views = models.IntegerField(default=0)
+    likes = models.IntegerField(default=0)
+    is_gif = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Trashcan(models.Model):
+    idx = models.AutoField(primary_key=True)
+    link = models.CharField(max_length=2048)
