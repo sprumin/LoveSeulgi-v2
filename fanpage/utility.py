b701d4bfb148bb197060d6f8b480af7877334307
sprumin <sprumin@gmail.com>
2019-11-23 17:40:51 +0900

    add get_photos detail settings

--- fanpage/utility.py
+++ fanpage/utility.py
@@ -1,11 +1,13 @@
 from fanpage.models import Photos
 
 
-def get_photos(width, height):
+def get_photos(width, height, max_width=None, max_height=None):
     """ return photos gte width, height"""
-    max_width = width + 100
-    max_height = height + 100
-    return Photos.objects.filter(
-        width__gte=width, width__lte=max_width,
-        height__gte=height, height__lte=max_height
-    )
+    photos = Photos.objects.filter(width__gte=width, height__gte=height)
+
+    if max_width:
+        photos = photos.filter(width__lte=max_width)
+    if max_height:
+        photos = photos.filter(height__lte=max_height)
+    
+    return photos
